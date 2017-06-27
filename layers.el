8b9f97b476a67328e031dda9a6267ed85d3e650c
Joshua Tomazin <joshua.tomazin@gmail.com>
2017-06-27 16:20:11 -0400

    Implement code folding and test macro

--- layers.el
+++ layers.el
@@ -12,7 +12,7 @@
       (layers--initialize)
     (layers--tear-down)))
 
-(defvar layer-labels (list)
+(defvar layer-labels (list "test" "doc")
   "the names of all the labels of layers")
 
 ;; This macro is the basic construct of layered programs.
@@ -20,11 +20,22 @@
 ;;  which contain extra information about the program which
 ;;  is not necessary for execution (e.g., tests, types,
 ;;  extra documentation, etc.)
-(defmacro layer (name &rest body)
+(defmacro layer (name symbol &rest body)
   "doc"
-  (declare (indent 1)) 
+  (declare (indent 2))
   (case name
-    (test `(list ,@body))
+    (test (cons 'list
+                (-map
+                 (lambda (expr)
+                   (if (member '-> expr) ; (a -> b) special form
+                       (let ((given (first expr))
+                             (expected (car (last expr))))
+                         `(not (assert (equal ,expected
+                                              (funcall #',symbol ,given)))))
+                     expr))
+                 body)))
+    (doc `(concat ,@body))
+    (spec ())                             ; clojure could use metadata
     (t    nil)))
 
 (defconst layers--macro-regex "\(layer[[:space:]\n]")
@@ -34,42 +45,67 @@
 ;;   ("b" -> 2)
 ;;   ("c" -> 3))
 
-;; (layer test
-;;   (string-match layers--macro-regex "(layer ")
-;;   (string-match layers--macro-regex "(layer test")
-;;   (string-match layers--macro-regex "(layer
-;; test")
-;;   (string-match layers--macro-regex "(layer
-;; ")
-;;   (string-match layers--macro-regex "(layer    name")
-;;   (not (string-match layers--macro-regex "(layers"))
-;;   (not (string-match layers--macro-regex "(layer"))
-;;   (not (string-match layers--macro-regex "layers"))
-;;   (not (string-match layers--macro-regex "(defun layer"))
-;;   (not (string-match layers--macro-regex "layer")))
+(layer test layers--macro-regex
+  (string-match layers--macro-regex "(layer ")
+  (string-match layers--macro-regex "(layer test")
+  (string-match layers--macro-regex "(layer
+test")
+  (string-match layers--macro-regex "(layer
+")
+  (string-match layers--macro-regex "(layer    name")
+  (not (string-match layers--macro-regex "(layers"))
+  (not (string-match layers--macro-regex "(layer"))
+  (not (string-match layers--macro-regex "layers"))
+  (not (string-match layers--macro-regex "(defun layer"))
+  (not (string-match layers--macro-regex "layer")))
 
-(defun layers--get-nth-sexpr (pos n)
-  "Returns the string of the N-th s-expression in the
-s-expression starting at POS. 1-indexed."
-  (let ((beg (1+ pos)))
-    (if (= n 1)
-        (buffer-substring-no-properties pos
-                                        (scan-sexps beg n))
-      (buffer-substring-no-properties (1+ (scan-sexps beg (- n 1)))
-                                      (scan-sexps beg n)))))
+(defun layers--get-nth-sexpr (beg n)
+  "Returns a string of the N-th s-expression in the s-expression
+starting at BEG. Zero-indexed."
+  (let ((end (scan-sexps beg 1)))
+    (symbol-name (nth n (car (read-from-string (buffer-substring beg end))))))
+  ;; (let ((beg (1+ pos)))
+  ;;   (if (= n 0)
+  ;;       (buffer-substring-no-properties pos
+  ;;                                       (scan-sexps beg n))
+  ;;     (buffer-substring-no-properties (1+ (scan-sexps beg n))
+  ;;                                     (scan-sexps beg (1+ n)))))
+  )
+
+(layer test layers--get-nth-sexpr
+  (-map (lambda (ol) (layers--get-nth-sexpr (overlay-start ol) 2))
+        (layers--get-layers-by-label 'test)))
+
+(defun layers--get-macro-symbol (pos-or-overlay)
+  "Gets the symbol associated with the layer macro specified by
+POS-OR-OVERLAY. POS-OR-OVERLAY can be a character position specifying
+the start of the layer sexpr, or the layer overlay covering it."
+  (let ((beg (cond ((overlayp pos-or-overlay) (overlay-start pos-or-overlay))
+                   ((integerp pos-or-overlay) pos-or-overlay)
+                   ((markerp pos-or-overlay) (marker-position pos-or-overlay))
+                   (t (error "Got %s; expected int or overlay"
+                             (type-of pos-or-overlay))))))
+    (layers--get-nth-sexpr beg 2)))
+
+(layer test layers--get-macro-symbol
+  (condition-case e
+      (layers--get-macro-symbol "illegal")
+    (error (string-match-p "string" (cadr e))))
+  (condition-case e
+      (layers--get-macro-symbol 'illegal)
+    (error (string-match-p "symbol" (cadr e)))))
 
 (defun layers--label (label)
   "Return the symbol associated with LABEL's layer (e.g., \"test\")"
-  (intern (concat (if (stringp label)
-                      label
-                    (symbol-name label))
-                  "-layer")))
+  (if (stringp label)
+      (intern label)
+    label))
 
-(layer test
-  (eq (layers--label "test") 'test-layer)
+(layer test layers--label
+  ("test" -> 'test)
+  ('test  -> 'test)
   (eq (layers--label "test") (layers--label 'test))
-  (eq (layers--label "test") (layers--label "test"))
-  (eq (layers--label 'test) (layers--label 'test)))
+  (eq (layers--label "test") (layers--label "test")))
 
 (defun layers--put-overlay-on-sexpr (beg)
   "Place a labeled layer overlay on the `layer' macro starting at position
@@ -77,7 +113,7 @@ BEG, or update it with a new label. Does nothing if a layer overlay
 of the correct type is already there."
   (let ((other-ols (overlays-at beg))
         (end (scan-sexps beg 1))
-        (label (layers--label (layers--get-nth-sexpr beg 2))))
+        (label (layers--label (layers--get-nth-sexpr beg 1))))
     (let ((overlay (or (-some (lambda (ol) ; overlay exists?
                                 (and (= beg (overlay-start ol))
                                      (= end (overlay-end ol))
@@ -86,46 +122,43 @@ of the correct type is already there."
                               other-ols)
                        (make-overlay beg (scan-sexps beg 1) ; or make a new one
                                      (current-buffer) t nil))))
-      (overlay-put overlay 'layer-label label))))
+      (overlay-put overlay 'layer-label label)
+      ;; (overlay-put overlay 'priority 1000000)
+      overlay)))
 
 (defun layers--get-layers-by-label (label)
   (-filter (lambda (ol)
              (eq (overlay-get ol 'layer-label) (layers--label label)))
-           (overlays-in 1 (point-max))))
+           (overlays-in (point-min) (point-max))))
 
 (defun layers--input-label ()
-  (completing-read "Layer: " layer-labels))
+  (layers--label (completing-read "Layer: " layer-labels)))
 
 (defun layers-hide-layer (label)
-  ;; (interactive (layers--input-label))
-  (let ((label (if (stringp label)
-                   (intern label)
-                 label)))
-    (-each (layers--get-layers-by-label label)
-      (lambda (ol)
+  ;; (interactive (layers--input-label)) 
+  (-each (layers--get-layers-by-label label)
+    (lambda (ol)
+      (let ((symbol (layers--get-macro-symbol ol)))
         (overlay-put ol 'invisible t)
         (overlay-put ol 'before-string
-                     (concat "(" (symbol-name label) "...)"))))))
+                     (concat "(" (symbol-name label) " "
+                             symbol " ...)"))))))
 
 (defun layers-show-layer (label)
   ;; (interactive (layers--input-label))
-  (let ((label (if (stringp label)
-                   (intern label)
-                 label)))
-    (-each (layers--get-layers-by-label label)
-      (lambda (ol)
-        (overlay-put ol 'invisible nil)
-        (overlay-put ol 'before-string nil)))))
+  (-each (layers--get-layers-by-label label)
+    (lambda (ol)
+      (overlay-put ol 'invisible nil)
+      (overlay-put ol 'before-string nil))))
 
 (defun layers-toggle-layer (label)
   (interactive (list (layers--input-label)))
-  (let ((label (if (stringp label)
-                   (intern label)
-                 label)))
-    (if (overlay-get (car (layers--get-layers-by-label label))
-                     'invisible)
-        (layers-show-layer label)
-      (layers-hide-layer label))))
+  (let* ((ol (car (layers--get-layers-by-label label)))
+         (invisible (overlay-get ol 'invisible)))
+    (cond
+     ((null ol) (message "No layers of type %s" label))
+     (invisible (layers-show-layer label))
+     (t         (layers-hide-layer label)))))
 
 (defun layers--find-layers ()
   "Return the character positions marking the beginning of all
@@ -148,10 +181,17 @@ layer macros in the buffer."
 
 (defun layers--initialize ()
   (add-hook 'after-change-functions #'layers--update-overlays t t)
-  (layers--update-overlays))
+  (layers--update-overlays)
+  'done)
 
 (defun layers--tear-down ()
-  (remove-hook 'after-change-functions #'layers--update-overlays t))
+  (remove-hook 'after-change-functions #'layers--update-overlays t)
+  (-each (-filter (lambda (ol)
+                    (overlay-get ol 'layer-label))
+                  (overlays-in (point-min) (point-max)))
+    (lambda (ol)
+      (delete-overlay ol)))
+  'done)
 
 ;; (layers--tear-down)
 ;; (layers--initialize)
